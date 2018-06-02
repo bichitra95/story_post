ed40cf95cf09165aae5c253882f4f47f2f8fa36e
Bichitra Kumar Sahoo <32828151+bichitra95@users.noreply.github.com>
2018-06-02 16:32:13 +0530

    Update __init__.py

--- apis/__init__.py
+++ apis/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:bichitra95@localhost:5432'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:passwd@localhost:5432'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     from apis.models import db
     db.init_app(app)
